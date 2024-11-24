90ccd623a9ee0b218e53c1ab09c4802568ee62fd
shenhaofang <lanjingren.com>
2024-11-24 15:38:02 +0800

    init

---
+++ qa_game_html/questions.js
@@ -0,0 +1,24 @@
+const questions = [
+    {
+        question: "2 + 2 等于几？",
+        options: ["3", "4", "5"],
+        answer: "4",
+        difficulty: 1
+    },
+    {
+        question: "HTTP 的默认端口号是？",
+        options: [],
+        answer: "80",
+        difficulty: 2
+    },
+    {
+        question: "Python 的作者是谁？",
+        options: [],
+        answer: "Guido van Rossum",
+        difficulty: 3
+    }
+];
+
+function autoMathQuestions(){
+    
+}
